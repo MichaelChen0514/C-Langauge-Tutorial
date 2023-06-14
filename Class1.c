806a357567daa9d04096e91029ae8e84573dfe0f
MichaelChen <66295030+MichaelChen0514@users.noreply.github.com>
2023-06-14 17:23:18 +0200

    Add files via upload

---
+++ Class1.c
@@ -0,0 +1,21 @@
+/*Include Package*/
+#include <stdio.h>
+
+/*Start of Main Programme*/
+int main(){
+    printf("Hello World!\n");
+    printf("today is Saturday\n");
+    printf("today is Sunday\n");
+   /*Introduce value variable a and b and give the value 20 to a and 5 to b*/
+    int a;
+    a = 20;
+    int b;
+    b = 5;
+    printf("The Value of a is:%d\n", a);
+    printf("The Value of b is:%d\n", b);
+    printf("The Value of a is %d, b is %d\n",a, b);
+    /*update the b value to -5*/
+    b = -5;
+    printf("The Value of b is updated to %d\n", b);
+    return 0;
+}
