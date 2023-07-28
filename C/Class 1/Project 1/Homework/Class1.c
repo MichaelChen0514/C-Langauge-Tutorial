f4b2b9d5947e23d8cbbe7736696ba77c68cef515
MichaelChen <66295030+MichaelChen0514@users.noreply.github.com>
2023-07-28 10:24:37 +0200

    Delete C directory

--- C/Class 1/Project 1/Homework/Class1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(){
-printf("Hello\nWorld\n\n!");
-printf("Hello World!\\n\n");
-int a = 5;
-printf("Hello \n %d \n", a);
-return 0;
-}
--- C/Class 1/Project 1/Homework/Untitled1.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main(){
-printf("Hello \n World\n\n");
-return 0;
-}
